f1fbe1deb572d9f460ada97825a252bf65b613f9
CatCoder <catcoderr@gmail.com>
2017-12-01 22:34:40 +0300

    Designed main interface.

--- src/main/java/ru/questcraft/pools/QuestPools.java
+++ src/main/java/ru/questcraft/pools/QuestPools.java
@@ -1,6 +1,31 @@
 package ru.questcraft.pools;
 
-public class QuestPools {
+import lombok.NonNull;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
 
+/**
+ * Основной класс для работы с пулом баз данных.
+ *
+ * @author CatCoder
+ */
+public interface QuestPools {
 
+    /**
+     * Создание стандартного экземпляра {@link Morphia}
+     * со стандартным маппингом объектов.
+     *
+     * @return стандартный экземпляр.
+     */
+    Morphia defaultMorphia();
+
+    /**
+     * Получение доступа к определенной базе данных по ключу.
+     * Если база данных публичная, то ключ не потребуется.
+     *
+     * @param morphia - инстанс морфии
+     * @param key     - ключ, может быть null.
+     * @return БД.
+     */
+    Datastore getDatastore(@NonNull Morphia morphia, String key);
 }
