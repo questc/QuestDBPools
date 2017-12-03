c6f75e82c6294d077991541f092adb7f0d27de49
CatCoder <catcoderr@gmail.com>
2017-12-03 19:05:35 +0200

    Changes

--- src/main/java/ru/questcraft/pools/SqlConnector.java
+++ src/main/java/ru/questcraft/pools/SqlConnector.java
@@ -4,14 +4,16 @@ import lombok.NonNull;
 import ru.questcraft.pools.query.AbstractQuery;
 import ru.questcraft.pools.query.QueryListener;
 
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 
 public interface SqlConnector {
 
     void execute(@NonNull AbstractQuery query, QueryListener<ResultSet> listener);
 
-    void metadata(@NonNull QueryListener<ResultSetMetaData> listener);
+    void metadata(@NonNull QueryListener<DatabaseMetaData> listener);
 
     String getDatabaseName();
+
+    void close();
 }
--- src/main/java/ru/questcraft/pools/query/AbstractQuery.java
+++ src/main/java/ru/questcraft/pools/query/AbstractQuery.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public abstract class AbstractQuery {
 
-    abstract ResultSet execute(Connection connection) throws SQLException;
+    public abstract ResultSet execute(Connection connection) throws SQLException;
 
     public static PreparedStatement fillObjects(@NonNull PreparedStatement ps, @NonNull Object... objects) throws SQLException {
         for(int i = 0; i < objects.length; i++){
