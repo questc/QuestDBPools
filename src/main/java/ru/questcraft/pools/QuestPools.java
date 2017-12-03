6817f95b8fce932db8542137c5f430f1ba0db6f1
CatCoder <catcoderr@gmail.com>
2017-12-03 17:19:48 +0200

    Use SPI

--- src/main/java/ru/questcraft/pools/QuestPools.java
+++ src/main/java/ru/questcraft/pools/QuestPools.java
@@ -1,6 +1,10 @@
 package ru.questcraft.pools;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 /**
  * Основной класс для работы с пулом баз данных MySQL и Redis
@@ -11,21 +15,44 @@ public interface QuestPools {
 
     /**
      * Получение пула баз данных Redis.
+     *
      * @return пул Redis.
      */
     JedisPool jedisPool();
 
     /**
+     * Авторизация экзмепляра класса {@link Jedis} для работы с запросами.
+     *
+     * @param jedis - инстанс Jedis
+     * @return авторизованный Jedis.
+     */
+    Jedis authenticate(Jedis jedis);
+
+    /**
      * Возвращает дефолтный конектор MySQL.
+     *
      * @return конектор.
      */
     SqlConnector defaultSqlConnector();
 
     /**
      * Возвращает специфический конектор для определенной БД.
+     *
      * @param database - имя БД
      * @return конектор.
      * @throws java.util.NoSuchElementException если конектор для такой БД не найден.
      */
     SqlConnector sqlConnector(String database);
+
+    /**
+     * Используя темную магию SPI достаем реализацию {@link QuestPools}
+     * @return реализацию, если она найдена, иначе {@link IllegalStateException}
+     */
+    static QuestPools instance(){
+        Iterator<QuestPools> iterator = ServiceLoader.load(QuestPools.class).iterator();
+
+        if(!iterator.hasNext()) throw new IllegalStateException("QPools implementations not found.");
+
+        return iterator.next();
+    }
 }
